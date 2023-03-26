fde3bcd4d46a8c998badd02430c24ffdb28ea38f
actiONmycoma <actionmycoma@gmail.com>
2023-03-26 18:02:20 +1000

    Добавил: музыку, звуки, текстуры

--- First_Independent_Game/Program.cs
+++ First_Independent_Game/Program.cs
@@ -12,11 +12,81 @@ namespace First_Independent_Game
 {
     internal class Program : Game
     {
+        public enum DogSounds
+        {
+            Bark,
+            Bark2,
+            Ouch,
+            Slurp,
+            Smack,
+            Whine,
+            Eat
+        }
+
+        public enum Food
+        {
+            Pizza,
+            Choco,
+            Burger
+        }
+
+        public enum Danger
+        {
+            Rock,
+            Knife
+        }
+
         static void Main(string[] args)
         {
-            InitWindow(1024, 798,"Catch Food!");
+            InitWindow(1024, 690, "Catch Food!");
 
-            string background = LoadTexture("foodtrack.png");
+            string backgroundMusic = LoadMusic("slow_8_Bit_Retro_Funk_background.wav");
+
+            string[] dogSounds =
+            {
+                LoadSound("bark.wav"),
+                LoadSound("bark2.wav"),
+                LoadSound("ouch.wav"),
+                LoadSound("slurp.wav"),
+                LoadSound("smack.wav"),
+                LoadSound("whine.wav"),
+                LoadSound("eat.wav")
+            };
+
+            string backgroundImage = LoadTexture("foodtrack.png");
+            string blurImage = LoadTexture("blur.png");
+
+            string dogSitImage = LoadTexture("no_move_sit.png");
+            string[] dogRightMove =
+            {
+                LoadTexture("move_right_0.png"),
+                LoadTexture("move_right_1.png"),
+                LoadTexture("move_right_2.png"),
+                LoadTexture("move_right_3.png")
+            };
+            string[] dogLeftMove =
+            {
+                LoadTexture("move_left_0.png"),
+                LoadTexture("move_left_1.png"),
+                LoadTexture("move_left_2.png"),
+                LoadTexture("move_left_3.png")
+            };
+
+            string[] foodImage =
+            {
+                LoadTexture("pizza.png"),
+                LoadTexture("choco.png"),
+                LoadTexture("burger.png")
+            };
+            string[] dangerImage =
+            {
+                LoadTexture("rock.png"),
+                LoadTexture("knife.png")                
+            };
+
+            string healthBar = LoadTexture("health_bar.png");
+
+            PlayMusic(backgroundMusic, 7);
 
             while (true)
             {
@@ -24,7 +94,8 @@ namespace First_Independent_Game
 
                 ClearWindow();
 
-                DrawSprite(background, 0, 0);
+                DrawSprite(backgroundImage, 0, 0);
+
 
                 DisplayWindow();
 
