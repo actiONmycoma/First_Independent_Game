bf9296d3bca5b709d589428f072cfcfa42435c4a
actiONmycoma <actionmycoma@gmail.com>
2023-03-26 22:05:27 +1000

    Добавил спавн еды и опасности, и их падение

--- First_Independent_Game/Program.cs
+++ First_Independent_Game/Program.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using SFML.Learning;
-using SFML.System;
 using SFML.Window;
+using SFML.Learning;
+using System.Collections.Generic;
 
 namespace First_Independent_Game
 {
     internal class Program : Game
     {
-        public enum DogSounds
+        private enum DogSounds
         {
             Bark,
             Bark2,
@@ -23,20 +18,20 @@ namespace First_Independent_Game
             Eat
         }
 
-        public enum Food
+        private enum Food
         {
             Pizza,
             Choco,
             Burger
         }
 
-        public enum Danger
+        private enum Danger
         {
             Rock,
             Knife
         }
 
-        static string[] dogRightMove =
+        private static string[] dogRightMove =
         {
             LoadTexture("move_right_0.png"),
             LoadTexture("move_right_1.png"),
@@ -44,25 +39,39 @@ namespace First_Independent_Game
             LoadTexture("move_right_3.png"),
             LoadTexture("no_move_sit_right.png")
         };
-        static string[] dogLeftMove =
+        private static string[] dogLeftMove =
         {
             LoadTexture("move_left_0.png"),
             LoadTexture("move_left_1.png"),
             LoadTexture("move_left_2.png"),
             LoadTexture("move_left_3.png"),
             LoadTexture("no_move_sit_left.png")
-
         };
 
-        static float dogX = 395;
-        static float dogY = 560;
-        static float dogColliderX = dogX - 10;
-        static float dogColliderY = dogY - 10;
-        static int colliderHeight = 120;
-        static int colliderWidth = 130;
+        private static string[] foodImage =
+        {
+            LoadTexture("pizza.png"),
+            LoadTexture("choco.png"),
+            LoadTexture("burger.png")
+        };
 
-        static int dogDirection = 0;
-        static float dogSpeed = 400;
+        private static string[] dangerImage =
+        {
+            LoadTexture("rock.png"),
+            LoadTexture("knife.png")
+        };
+
+        private static float dogX = 395;
+        private static float dogY = 560;
+        private static float dogColliderX = dogX - 10;
+        private static float dogColliderY = dogY - 10;
+        private static int colliderHeight = 120;
+        private static int colliderWidth = 130;
+
+        private static int dogDirection = 0;
+        private static float dogSpeed = 400;
+
+        private static float dropSpeed = 400;
 
         static void Main(string[] args)
         {
@@ -86,35 +95,27 @@ namespace First_Independent_Game
             string backgroundImage = LoadTexture("foodtrack.png");
             string blurImage = LoadTexture("blur.png");
 
-
-
-            string[] foodImage =
-            {
-                LoadTexture("pizza.png"),
-                LoadTexture("choco.png"),
-                LoadTexture("burger.png")
-            };
-            string[] dangerImage =
-            {
-                LoadTexture("rock.png"),
-                LoadTexture("knife.png")
-            };
-
             string healthBar = LoadTexture("health_bar.png");
 
 
-            //-----основной цикл игры-----
+
+            float[] newDropPosition;
+            List<string> drops = new List<string>();
+            List<float[]> dropPositions = new List<float[]>();
 
             int score = 0;
 
             bool isNewGame = true;
             bool isExit = false;
 
+            //-----основной цикл игры-----
             while (!isExit)
             {
 
 
                 PlayMusic(backgroundMusic, 7);
+
+                int timeCount = 0;
 
                 while (true)
                 {
@@ -122,12 +123,26 @@ namespace First_Independent_Game
 
                     DogMove();
 
+                    if (timeCount++ % 100 == 0)
+                    {
+                        dropPositions.Add(GetDropStartPosition());
+                        drops.Add(GetRandomDrop());
+                    }
+
+                    for (int i = 0; i < dropPositions.Count; i++)
+                    {
+                        DropMove(dropPositions[i]);
+                    }
+
                     ClearWindow();
 
                     DrawSprite(backgroundImage, 0, 0);
 
-                    //food
-                    //danger
+                    //drop
+                    for (int i = 0; i < drops.Count; i++)
+                    {
+                        DrawDrop(dropPositions[i], drops[i]);
+                    }
 
                     //dog                    
                     DrawDog();
@@ -139,7 +154,7 @@ namespace First_Independent_Game
 
                     //score
                     SetFillColor(0, 0, 0);
-                    DrawText(70, 80, "SCORE:", 30);
+                    DrawText(70, 80, $"SCORE: {score}", 30);
 
                     DisplayWindow();
 
@@ -155,11 +170,14 @@ namespace First_Independent_Game
             if (dogDirection == -1) DrawSprite(dogLeftMove[4], dogX, dogY);
             if (dogDirection == 0) DrawSprite(dogRightMove[4], dogX, dogY);
 
-            if (dogDirection == 1) DrawSprite(dogLeftMove[1], dogX, dogY);
+            if (dogDirection == 1) DrawSprite(dogLeftMove[0], dogX, dogY);
 
+            if (dogDirection == 2) DrawSprite(dogRightMove[0], dogX, dogY);
+        }
 
-            if (dogDirection == 2) DrawSprite(dogRightMove[1], dogX, dogY);
-
+        static void DrawDrop(float[] position, string image)
+        {
+            DrawSprite(image, position[0], position[1]);
         }
 
         static void DogMove()
@@ -181,6 +199,30 @@ namespace First_Independent_Game
                 if (dogDirection == 1) dogDirection = -1;
                 if (dogDirection == 2) dogDirection = 0;
             }
+
+        }
+
+        static void DropMove(float[] dropPosition)
+        {
+            dropPosition[1] += dropSpeed * DeltaTime;
+        }
+
+        static float[] GetDropStartPosition()
+        {
+            Random rnd = new Random();
+
+            return new float[] { rnd.Next(25, 1000), -50 };
+        }
+        static string GetRandomDrop()
+        {
+            Random rnd = new Random();
+
+            int food = rnd.Next(2);
+
+            if (food == 0)
+                return dangerImage[rnd.Next(dangerImage.Length)];
+            else
+                return foodImage[rnd.Next(foodImage.Length)];
 
         }
     }
