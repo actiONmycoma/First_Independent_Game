7b8cd11ccedd3776d37a8f44d6a9ad69542bb6b7
actiONmycoma <actionmycoma@gmail.com>
2023-03-26 13:08:15 +1000

    Add project files.

---
+++ First_Independent_Game/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Learning;
+using SFML.System;
+using SFML.Window;
+
+namespace First_Independent_Game
+{
+    internal class Program : Game
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
