2c4f9549823f8512914592dcefaae11204b502cd
actiONmycoma <actionmycoma@gmail.com>
2023-03-26 20:15:58 +1000

    Добавил элементы интерфейса и базовые движения

--- First_Independent_Game/Program.cs
+++ First_Independent_Game/Program.cs
@@ -36,9 +36,39 @@ namespace First_Independent_Game
             Knife
         }
 
+        static string[] dogRightMove =
+        {
+            LoadTexture("move_right_0.png"),
+            LoadTexture("move_right_1.png"),
+            LoadTexture("move_right_2.png"),
+            LoadTexture("move_right_3.png"),
+            LoadTexture("no_move_sit_right.png")
+        };
+        static string[] dogLeftMove =
+        {
+            LoadTexture("move_left_0.png"),
+            LoadTexture("move_left_1.png"),
+            LoadTexture("move_left_2.png"),
+            LoadTexture("move_left_3.png"),
+            LoadTexture("no_move_sit_left.png")
+
+        };
+
+        static float dogX = 395;
+        static float dogY = 560;
+        static float dogColliderX = dogX - 10;
+        static float dogColliderY = dogY - 10;
+        static int colliderHeight = 120;
+        static int colliderWidth = 130;
+
+        static int dogDirection = 0;
+        static float dogSpeed = 400;
+
         static void Main(string[] args)
         {
             InitWindow(1024, 690, "Catch Food!");
+
+            SetFont("Webcomic.ttf");
 
             string backgroundMusic = LoadMusic("slow_8_Bit_Retro_Funk_background.wav");
 
@@ -56,21 +86,7 @@ namespace First_Independent_Game
             string backgroundImage = LoadTexture("foodtrack.png");
             string blurImage = LoadTexture("blur.png");
 
-            string dogSitImage = LoadTexture("no_move_sit.png");
-            string[] dogRightMove =
-            {
-                LoadTexture("move_right_0.png"),
-                LoadTexture("move_right_1.png"),
-                LoadTexture("move_right_2.png"),
-                LoadTexture("move_right_3.png")
-            };
-            string[] dogLeftMove =
-            {
-                LoadTexture("move_left_0.png"),
-                LoadTexture("move_left_1.png"),
-                LoadTexture("move_left_2.png"),
-                LoadTexture("move_left_3.png")
-            };
+
 
             string[] foodImage =
             {
@@ -81,25 +97,89 @@ namespace First_Independent_Game
             string[] dangerImage =
             {
                 LoadTexture("rock.png"),
-                LoadTexture("knife.png")                
+                LoadTexture("knife.png")
             };
 
             string healthBar = LoadTexture("health_bar.png");
 
-            PlayMusic(backgroundMusic, 7);
 
-            while (true)
+            //-----основной цикл игры-----
+
+            int score = 0;
+
+            bool isNewGame = true;
+            bool isExit = false;
+
+            while (!isExit)
             {
-                DispatchEvents();
-
-                ClearWindow();
-
-                DrawSprite(backgroundImage, 0, 0);
 
 
-                DisplayWindow();
+                PlayMusic(backgroundMusic, 7);
 
-                Delay(1);
+                while (true)
+                {
+                    DispatchEvents();
+
+                    DogMove();
+
+                    ClearWindow();
+
+                    DrawSprite(backgroundImage, 0, 0);
+
+                    //food
+                    //danger
+
+                    //dog                    
+                    DrawDog();
+
+                    //health
+                    DrawSprite(healthBar, 10, 20);
+                    SetFillColor(240, 39, 39);
+                    FillRectangle(81, 36, 503, 30);
+
+                    //score
+                    SetFillColor(0, 0, 0);
+                    DrawText(70, 80, "SCORE:", 30);
+
+                    DisplayWindow();
+
+                    Delay(1);
+                }
+
+            }
+
+        }
+
+        static void DrawDog()
+        {
+            if (dogDirection == -1) DrawSprite(dogLeftMove[4], dogX, dogY);
+            if (dogDirection == 0) DrawSprite(dogRightMove[4], dogX, dogY);
+
+            if (dogDirection == 1) DrawSprite(dogLeftMove[1], dogX, dogY);
+
+
+            if (dogDirection == 2) DrawSprite(dogRightMove[1], dogX, dogY);
+
+        }
+
+        static void DogMove()
+        {
+            if (GetKey(Keyboard.Key.A) == true)
+            {
+                dogX -= dogSpeed * DeltaTime;
+                dogDirection = 1;
+            }
+
+            if (GetKey(Keyboard.Key.D) == true)
+            {
+                dogX += dogSpeed * DeltaTime;
+                dogDirection = 2;
+            }
+
+            if (GetKeyDown(Keyboard.Key.S) == true)
+            {
+                if (dogDirection == 1) dogDirection = -1;
+                if (dogDirection == 2) dogDirection = 0;
             }
 
         }
